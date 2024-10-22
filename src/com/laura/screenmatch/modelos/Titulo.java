554397f64e70a518604906c716cb9491bdaec508
LauraFalla1 <fallalaura350gmail.com>
2024-10-22 00:02:55 -0500

    imprementando java record para utilizar un json

--- src/com/laura/screenmatch/modelos/Titulo.java
+++ src/com/laura/screenmatch/modelos/Titulo.java
@@ -1,8 +1,12 @@
 package com.laura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
 
+    @SerializedName("Title")
     private String nombre;
+    @SerializedName("Year")
     private int fechaDeLanzamiento;
     private int duracionEnMinutos;
     private boolean incluidoEnElPlan;
@@ -12,6 +16,12 @@ public class Titulo implements Comparable<Titulo>{
     public Titulo(String nombre, int fechaDeLanzamiento) {
         this.nombre = nombre;
         this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public Titulo(TituloOmdb miTituloOmdb) {
+        this.nombre = miTituloOmdb.title();
+        this.fechaDeLanzamiento = Integer.valueOf(miTituloOmdb.year());
+        this.duracionEnMinutos = Integer.valueOf(miTituloOmdb.runtime().substring(0,2));
     }
 
     public String getNombre() {
@@ -67,5 +77,12 @@ public class Titulo implements Comparable<Titulo>{
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return "nombre= '" + nombre + '\'' +
+                ", fechaDeLanzamiento= " + fechaDeLanzamiento +
+                ", duracion= "+duracionEnMinutos;
     }
 }
